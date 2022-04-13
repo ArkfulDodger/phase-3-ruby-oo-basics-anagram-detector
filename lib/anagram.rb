e247f7599d8b210f050456a139c831879feec8ef
Noah Reece <noah.w.reece@gmail.com>
2022-04-13 13:06:37 -0400

    commit tree c738d4905fcc43bc572888ca222c18f382e7632f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    # attr_accessor :word
+
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        arr.select { |str| anagram?(str) }
+    end
+    
+    private
+
+    def anagram?(str)
+        letter_counts(@word) == letter_counts(str)
+    end
+    
+    def letter_counts(str)
+        ltr_count = {}
+        str.split('').each { |a| ltr_count[a] ? ltr_count[a] += 1 : ltr_count[a] = 1 }
+        ltr_count
+    end
+end
+
+listen = Anagram.new('listen')
